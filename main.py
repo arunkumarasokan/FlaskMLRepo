95fcd4db0aeceb993101e1d4800c565b49e0f318
arunkumarasokan <51292008+arunkumarasokan@users.noreply.github.com>
2020-08-14 12:55:59 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,26 @@
-import uvicorn
-from fastapi import FastAPI
+
+from flask import Flask, request, jsonify, render_template, url_for
 import pickle
 
 
-#openModel
+app = Flask(__name__)
 pickle_in = open("ExxonOil.pickle", "rb")
 linear = pickle.load(pickle_in)
 
+@app.route('/')
+def home():
+    return 'Hello World'
+    #return render_template('home.html')
+    #return render_template('index.html')
 
-app = FastAPI()
 
-@app.get('/')
-
-async def index():
-    return{"text": "heloo there"}
-
-@app.get('/items/{oilprice}')
-async def get_items(oilprice):
-    return {"oil price": oilprice}
-
-#ML Aspect
-
-@app.get('/predict/{oilprice}')
-async def predict(oilprice):
-    op = float(oilprice)
-    prediction = linear.predict([[op]])
-    predicted_value = prediction[0][0]
-    return {"oil_price":oilprice, "EXXON Price" : predicted_value}
+@app.route('/predict/<oilprice>', methods=['POST', 'GET'])
+def predict(oilprice):
+    if request.method == 'GET':
+        op = float(oilprice)
+        prediction = linear.predict([[op]])
+        predicted_value = prediction[0][0]
+        return {"oil_price": oilprice, "EXXON Price": predicted_value}
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    app.run(debug=True)
